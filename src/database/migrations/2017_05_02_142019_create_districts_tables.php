a1384dd88965c600f512fa759d1c139231f7fa1b
Hajar Aswad <hajaraswadkom@gmail.com>
2022-10-19 17:22:45 +0700

    update: migration

--- src/database/migrations/2017_05_02_142019_create_districts_tables.php
+++ src/database/migrations/2017_05_02_142019_create_districts_tables.php
@@ -22,11 +22,11 @@ class CreateDistrictsTables extends Migration
     {
         Schema::create('districts', function(Blueprint $table){
             $table->char('id', 7)->index();
-            $table->char('regency_id', 4);
+            $table->char('city_id', 4);
             $table->string('name', 50);
-            $table->foreign('regency_id')
+            $table->foreign('city_id')
                 ->references('id')
-                ->on('regencies')
+                ->on('cities')
                 ->onUpdate('cascade')->onDelete('restrict');
         });
     }
